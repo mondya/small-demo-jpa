2d59064361fdb8d02ede5208d5aca6715d0f3820
xhh <xhh19990210@gmail.com>
2023-04-30 10:51:28 +0800

    add groovy

--- src/main/java/com/xhh/smalldemojpa/domain/user/User.java
+++ src/main/java/com/xhh/smalldemojpa/domain/user/User.java
@@ -2,7 +2,6 @@ package com.xhh.smalldemojpa.domain.user;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.ToString;
 
 import javax.persistence.*;
 import java.io.Serializable;
