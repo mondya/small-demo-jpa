7f7eea6bfbbce8a8c744a8779ea704e0790984c5
xhh <xhh19990210@gmail.com>
2023-05-17 20:56:52 +0800

    ⚡️

--- src/main/java/com/xhh/smalldemojpa/config/SecurityConfig.java
+++ src/main/java/com/xhh/smalldemojpa/config/SecurityConfig.java
@@ -4,10 +4,12 @@ import com.xhh.smalldemojpa.filter.AuthenticationTokenFilter;
 import com.xhh.smalldemojpa.handler.RestAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -27,12 +29,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf().disable()
+        // 不需要csrf,session
+        httpSecurity.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeHttpRequests(
                         (authz) -> 
-                                authz.antMatchers("/api/1.0/login").permitAll()
+                                authz.antMatchers("/api/1.0/login").permitAll().antMatchers(HttpMethod.OPTIONS).permitAll()
                                         .anyRequest().authenticated()
                 ).httpBasic(Customizer.withDefaults());
+
+        // 禁用缓存
+        httpSecurity.headers().cacheControl();
         
         httpSecurity.addFilterBefore(authenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
         httpSecurity.exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint);
