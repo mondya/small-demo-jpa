ce73c1a255a6e8721e248211d24cc5434afcc440
xhh <xhh19990210@gmail.com>
2023-08-17 23:51:27 +0800

    add HttpUtil

---
+++ src/main/java/com/xhh/smalldemojpa/utils/HttpUtil.java
@@ -0,0 +1,8 @@
+package com.xhh.smalldemojpa.utils;
+
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+
+public class HttpUtil {
+    
+    private static PoolingHttpClientConnectionManager pool;
+}
