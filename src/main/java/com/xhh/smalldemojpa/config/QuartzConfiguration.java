88f69998a53c059785942ebed3daef97470ff0e0
xhh <xhh19990210@gmail.com>
2025-01-12 21:12:24 +0800

    quartz分布式配置

---
+++ src/main/java/com/xhh/smalldemojpa/config/QuartzConfiguration.java
@@ -0,0 +1,15 @@
+package com.xhh.smalldemojpa.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+
+@Configuration
+public class QuartzConfiguration {
+    
+//    @Bean
+//    public SchedulerFactoryBean schedulerFactoryBean() {
+//        SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
+//        return schedulerFactoryBean;
+//    }
+}
